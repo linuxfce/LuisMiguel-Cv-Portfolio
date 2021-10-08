0634dd1c7b3f7de2ca7dd506af7555d1de47f4e9
Mig T <linuxfce@users.noreply.github.com>
2021-10-08 15:42:49 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ $(function(){
   var backgroundImg = [
     'https://images.unsplash.com/photo-1483125078247-1a358ff363d3?dpr=1.2000000476837158&auto=format&fit=crop&w=1500&h=1000&q=80&cs=tinysrgb&crop=&bg='
   ];
-
+/*
   $('body').css('background-image', 'url(' + backgroundImg[0] + ')');
-
+*/
 })
