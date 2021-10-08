7967738f334944800e8a7e5b535883229f8066b6
Mig T <linuxfce@users.noreply.github.com>
2021-10-08 15:51:11 +0100

    a

--- js/index.js
+++ js/index.js
@@ -4,6 +4,6 @@ $(function(){
     'https://images.unsplash.com/photo-1483125078247-1a358ff363d3?dpr=1.2000000476837158&auto=format&fit=crop&w=1500&h=1000&q=80&cs=tinysrgb&crop=&bg='
   ];
 /*
-  $('body').css('background-image', 'url(' + backgroundImg[0] + ')');
+  $('body').css('background-image', 'url(' + backgroundImg[2] + ')');
 */
 })
